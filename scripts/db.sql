046cbdbe23a556d2de3dd64204bd5ed75fa97118
ihar.artsiomenka <ihar.artsiomenka@gmail.com>
2021-11-18 17:04:54 +0300

    add scripts and girectories

---
+++ scripts/db.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE IF NOT EXISTS bookstore;
+GRANT ALL PRIVILEGES ON bookstore.* TO 'root'@'%' IDENTIFIED BY 'mysql';
+USE bookstore;
+CREATE TABLE Genre
+(
+    id                          bigint(20)   UNSIGNED NOT NULL,
+    name                        varchar(99)  DEFAULT NULL,
+    PRIMARY KEY (id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+CREATE TABLE Book
+(
+    id                          bigint(20)   UNSIGNED NOT NULL,
+    name                        varchar(99)  DEFAULT NULL,
+    genre_id                    bigint(20)   UNSIGNED NOT NULL,
+    price                       double(10,2) UNSIGNED DEFAULT 0,
+    amount                      bigint(20)   UNSIGNED DEFAULT 0,
+    PRIMARY KEY (id),
+    FOREIGN KEY (genre_id)  REFERENCES Genre (id)
+) ENGINE = InnoDB DEFAULT CHARSET = utf8mb4;
+
+INSERT INTO Genre
+VALUES (1, 'Adventure');
+INSERT INTO Genre
+VALUES (2, 'Classics');
+INSERT INTO Genre
+VALUES (3, 'Fantasy');
+
+INSERT INTO Book
+VALUES (1, 'Don Quixote', 1, 100, 5);
+INSERT INTO Book
+VALUES (2, 'Moby Dick', 2, 200, 10);
+INSERT INTO Book
+VALUES (3, 'Game of thrones', 3, 300, 10);
+INSERT INTO Book
+VALUES (4, 'Dracula', 2, 300, 20);
